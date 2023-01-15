cc78cf75eb4f1489e94435dd231a35216f2570a1
makabaka1880 <makabaka1880@outlook.com>
2023-01-15 20:43:06 +0800

    Initialized scene

--- BrickBreaker/GameViewController.swift
+++ BrickBreaker/GameViewController.swift
@@ -12,14 +12,23 @@ import SceneKit
 class GameViewController: UIViewController {
 
     var scnView: SCNView!
+    var scnScene: SCNScene!
+    var game = GameHelper.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupScene()
+        scnView.allowsCameraControl = true
     }
     
     // MARK: SetupScene()
     func setupScene() {
+        scnScene = .init(named: "Breaker.scnassets/Scenes/Game.scn")!
         scnView = self.view as! SCNView
         scnView.delegate = self
+        scnView.scene = scnScene
+        game.hudNode.position = .init(x: 0, y: 20, z: 0)
+        scnScene.rootNode.addChildNode(game.hudNode)
     }
     override var prefersStatusBarHidden: Bool {
         return true
@@ -33,6 +42,6 @@ class GameViewController: UIViewController {
 
 extension GameViewController: SCNSceneRendererDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        
+        game.updateHUD()
     }
 }
